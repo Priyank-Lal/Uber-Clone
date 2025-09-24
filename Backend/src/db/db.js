c5a30130d11e6b31d73747177c84af73ae4b64a1
Priyank lal <ratherdrag@gmail.com>
2025-09-24 12:23:16 +0530

    Connected to Database

---
+++ Backend/src/db/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const connectToDB = () => {
+  try {
+    mongoose.connect(process.env.MONGO_URI);
+
+    console.log("Connected to Database");
+  } catch (error) {
+    console.log("An error occurred while connecting to DB: ", error);
+  }
+};
+
+module.exports = connectToDB
